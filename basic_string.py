a636068bc700a57e117c3578a6bc522f55472f77
PoloCancha <pabscampana@gmail.com>
2026-02-03 17:15:37 +0100

    String examples

---
+++ basic_string.py
@@ -0,0 +1,21 @@
+s1 = "hello world "
+s2 = 'hello world '
+print(s1)
+print(s2)
+
+s1 = ("lksj832ññ=·") # it can have weird elements
+print(s1)
+
+# we can index a string
+s = "banana"
+print(s[1]) # indexing starts from zero
+print(s[0]) # first letter
+print(s[10//2])
+
+#you can also index by negatives
+print(s[-1])
+print(s[-6]) # same as s[0]
+
+# len function will tell you the amount of characters
+print(s, len(s))
+
---
+++ string_operators.py
@@ -0,0 +1,10 @@
+# + will perform string concatenation
+s1 = "hello"
+s2 = "bye"
+print(s1 + s2)
+print(s2 + s1)
+print(s1 + " " + s2)
+
+# you can multiply string by an integer
+print(3*s2) #nsync song
+print(s1 + " " + s2 + "!"*10)
